a09182b66bbd841f10920e3bdd7e9f97015b50e5
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-10 23:05:39 +0200

    update Formulaire

--- 9-LesFormulairesEtJs/main.js
+++ 9-LesFormulairesEtJs/main.js
@@ -11,19 +11,36 @@ error.style.display = "none";
 // Etape 3 - Générer un nombre aléatoire
 
 let nombreAleatoire = Math.floor(Math.random() * 1001);
+let coups = 0;
+let nombreChoisi;
 
 // Etape 4 - Vérifier que l'utilisateur donne bien un nombre
 
 input.addEventListener('keyup', () => {
     if(isNaN(input.value)){
+        // Afficher le message d'erreur.
         error.style.display = "inline";
     }else {
+        // Cacher le message d'erreur.
         error.style.display = "none";
     }
 });
 
 // Etape 5 - Agir à l'envoi du formulaire
 
+formulaire.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if(isNaN(input.value) || input.value == ""){
+       input.style.borderColor = "red";
+    }
+    else {
+        coups++;
+        input.style.borderColor = "silver";
+        nombreChoisi = input.value;
+        input.value = "";
+        verifier(nombreChoisi);
+    }
+})
 
 
 // Etape 6 - Créer la fonction vérifier
