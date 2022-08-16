21bd47b55eff507e3d957d0cdf595e55f54ef280
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-16 17:38:52 +0200

    update Fight Simulateur

--- 11-Projet#FightSimulateur/main.js
+++ 11-Projet#FightSimulateur/main.js
@@ -1,49 +1,111 @@
 class Personnage {
-  constructor(pseudo, classe, sante, attaque, niveau) {
+  constructor(pseudo, classe, sante, attaque) {
     this.pseudo = pseudo;
     this.classe = classe;
     this.sante = sante;
     this.attaque = attaque;
-    this.niveau = niveau;
+    this.niveau = 1;
   }
-  evoluer(){
-      console.log(this.pseudo + " passe au  niveau " + (this.niveau+1));
-  };
 
-  verifierSante(){
-    if(this.sante <= 0) {
-        this.sante = 0;
-        console.log(this.pseudo + " a perdu !");
-    }
-  };
-  
-  informations() {
-    console.log(
+  get informations() {
+    return (
       this.pseudo +
-        " " +
-        this.classe +
-        " a " +
-        this.sante +
-        " points de vie et il est au niveau " +
-        this.niveau
+      " (" +
+      this.classe +
+      ") a " +
+      this.sante +
+      " points de vie et il est au niveau " +
+      this.niveau +
+      " ."
     );
+  }
+
+  evoluer() {
+    this.niveau++;
+    console.log(this.pseudo + " passe au  niveau " + this.niveau + " !");
+  }
+
+  verifierSante() {
+    if (this.sante <= 0) {
+      this.sante == 0;
+      console.log(this.pseudo + " a perdu !");
+    }
   }
 }
 
 class Magicien extends Personnage {
-    constructor(ghandour){
-        super(ghandour, magicien, 170, 90);
-        this.ghandour = ghandour;
-        this.magicien = magicien;
-        this.sante= 170;
-        this.attaque=  90;
-    }
-    attaquer(personnage){
-        personnage.sante -= this.attaque;
-        console.log(this.pseudo + " attaque "+ personnage.pseudo + " " + this.attaque + " dégâts");
-    }
+  constructor(pseudo) {
+    super(pseudo, "magicien", 170, 90);
+  }
+
+  attaquer(personnage) {
+    personnage.sante -= this.attaque;
+    console.log(
+      this.pseudo +
+        " attaque " +
+        personnage.pseudo +
+        " en lançant un sort (" +
+        this.attaque +
+        " dégâts)"
+    );
+    this.evoluer();
+    personnage.verifierSante();
+  }
+  coupSpecial(personnage) {
+    personnage.sante -= this.attaque * 5;
+    console.log(
+      this.pseudo +
+        " attaque avec son coup spécial puissance des arcanes " +
+        personnage.pseudo +
+        " (" +
+        this.attaque * 5 +
+        " dégâts)"
+    );
+    this.evoluer();
+    personnage.verifierSante();
+  }
 }
 
-var personnage = new Magicien(ghandour, magicien, 170, 90)
-personnage.evoluer();
-personnage.verifierSante();
+class Guerrier extends Personnage {
+  constructor(pseudo) {
+    super(pseudo, "guerrier", 350, 50);
+  }
+  attaquer(personnage) {
+    personnage.sante -= this.attaque;
+    console.log(
+      this.pseudo +
+        " attaque " +
+        personnage.pseudo +
+        " avec son épée (" +
+        this.attaque +
+        " dégâts)"
+    );
+    this.evoluer();
+    personnage.verifierSante();
+  }
+  coupSpecial(personnage) {
+    personnage.sante -= this.attaque * 5;
+    console.log(
+      this.pseudo +
+        " attaque avec ses haches de geurre " +
+        personnage.pseudo +
+        " (" +
+        this.attaque * 5 +
+        " dégâts)"
+    );
+    this.evoluer();
+    personnage.verifierSante();
+  }
+}
+
+var gandalf = new Magicien("Gandalf");
+var thor = new Guerrier("Thor");
+console.log(thor.informations);
+console.log(gandalf.informations);
+gandalf.attaquer(thor);
+console.log(thor.informations);
+thor.attaquer(gandalf);
+console.log(gandalf.informations);
+gandalf.coupSpecial(thor);
+
+
