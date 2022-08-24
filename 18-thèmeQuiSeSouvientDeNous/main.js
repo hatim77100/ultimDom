d1eb8da4df5deb2413057ea17540e4f796ad6276
hatim77100 <najoua-hatim@hotmail.fr>
2022-08-24 22:11:01 +0200

    theme sombre

--- 18-thèmeQuiSeSouvientDeNous/main.js
+++ 18-thèmeQuiSeSouvientDeNous/main.js
@@ -1,11 +1,27 @@
 let btn = document.getElementById('mode');
 let span = document.querySelector('span');
 
-btn.addEventListener('click', ()=>{
-    if(document.body.classList.contains("dark") == true) {
+if(localStorage.getItem("theme")){
+   if(localStorage.getItem("theme") == "sombre") {
+       modeSombre();
+    } 
+}
 
-        document.body.classList.add("white");
-    }else {
-        document.body.classList.add("dark");
-    }
+btn.addEventListener('click', ()=>{ 
+   if(document.body.classList.contains("dark")){
+    // mode clair
+    document.body.className ="";
+    span.textContent = "thème sombre";
+    localStorage.setItem("thème", "clair");
+   }
+   else {
+    //je peux passer au mode sombre
+    modeSombre()
+   }
 });
+
+function modeSombre(){
+    document.body.className = "dark";
+    span.textContent = "thème clair";    
+    localStorage.setItem("theme", "sombre");
+};
